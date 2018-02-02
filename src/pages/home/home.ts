034f64a626b13e333edcaa7952c72a68a9c06743
LionPK <praneed.kla@northbkk.ac.th>
2018-02-02 16:46:07 +0700

    add new page

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,11 +1,14 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
+import { NorthPage} from '../north/north';
 
 @Component({
   selector: 'page-home',
   templateUrl: 'home.html'
 })
 export class HomePage {
+
+  NorthPage:any = NorthPage;
 
   constructor(public navCtrl: NavController) {
 
